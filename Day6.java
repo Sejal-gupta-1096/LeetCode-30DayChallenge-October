0c4f9beab5958f1af5a70e5b4be66889b85a1903
Sejal-gupta-1096 <sejal1096.cse18@chitkara.edu.in>
2020-10-07 10:08:34 +0530

    Da6 challenge completed

---
+++ Day6.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        
+        if(root == null){
+            TreeNode node = new TreeNode(val);
+            return node;
+        }
+        
+        if(root.val > val){
+            root.left = insertIntoBST(root.left , val);
+        }else{
+            root.right = insertIntoBST(root.right , val);
+        }
+        
+        return root;
+    }
+}
